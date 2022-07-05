14824eaef9cd717a5ff2fa7d783d474a70460541
Giovanne Marrone <giovannemarrone@gmail.com>
2022-07-05 00:24:38 -0300

    automatized participant addition

--- main.js
+++ main.js
@@ -3,3 +3,29 @@ function toggleDarkMode() {
 
   element.classList.toggle("dark-mode");
 }
+
+// vvv  Returns Participants vvv //
+
+async function returnParticipants() {
+  let promise = await fetch("participants.json")
+
+  const data = await promise.json()
+
+  let participantsList = document.getElementById('participantsList')
+  
+  for (i = 0; i < data.participants.length; i++) {
+    let participant = `
+                      <a class="participant" href="./Participants/index.html">
+                          <img class="participant-avatar" src="${data.participants[i].participantPhoto}" alt="Avatar">
+                          <p class="participant-name">${data.participants[i].participantName}</p>
+                      </a>
+    `
+    participantsList.innerHTML += participant
+    ;
+  }
+}
+
+returnParticipants()
+
+
+
